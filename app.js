ae3d35d7286c80ce27e6176873885a7e8625c302
khalidlotfi <khalidlotfi2005@gmail.com>
2024-01-22 11:09:29 +0100

    Snake shouldn't pass through the wal or itself

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let PIXEL = 10;
 
 let pixels = new Map();
 
+//  drawing the canvas
 function initializeCanvas() {
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
@@ -23,22 +24,14 @@ function initializeCanvas() {
   }
 }
 
-// console.log(pixels);
-
 initializeCanvas();
 
-function drawSnake(snake) {
-  let snakePositions = new Set();
-  for (let [top, left] of snake) {
-    let position = top + "_" + left;
-    snakePositions.add(position);
-  }
-  // console.log(snakePositions);
+function drawSnake() {
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
       let position = i + "_" + j;
       let pixel = pixels.get(position);
-      pixel.style.background = snakePositions.has(position) ? "black" : "white";
+      pixel.style.background = currentSnakePositions.has(position) ? "black" : "white";
     }
   }
 }
@@ -50,6 +43,14 @@ let currentSnake = [
   [0, 4],
   [0, 5],
 ];
+
+let currentSnakePositions = toPositionSet(currentSnake)
+
+/* const updateSnake = (nextSnake) => {
+  currentSnake = nextSnake
+  currentSnakePositions = toPositionSet(nextSnake);
+} */
+
 
 let moveRight = ([t, l]) => [t, l + 1];
 let moveLeft = ([t, l]) => [t, l - 1];
@@ -88,8 +89,6 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
-// console.log(directionQueue);
-
 const step = () => {
   currentSnake.shift();
   let head = currentSnake[currentSnake.length - 1];
@@ -97,16 +96,25 @@ const step = () => {
   let nextDirection = currentDirection;
   while (directionQueue.length > 0) {
     let candidateDirection = directionQueue.shift();
-    if (areOpposite(candidateDirection, currentDirection)) {
-      continue;
+    if (!areOpposite(candidateDirection, currentDirection)) {
+      nextDirection = candidateDirection;
+      break;
     }
-    nextDirection = candidateDirection;
-    break;
   }
   currentDirection = nextDirection;
+  // next head
   let nextHead = currentDirection(head);
+  // we check if the next head is a valid position
+  // we continue if it's not we stop the game
+  // we'll define a checkValidHead n stopGame functions below
+  if (!checkValidHead(currentSnakePositions, nextHead)) {
+    stopGame();
+    return;
+  }
+
   currentSnake.push(nextHead);
-  drawSnake(currentSnake);
+  currentSnakePositions = toPositionSet(currentSnake);
+  drawSnake();
 };
 
 const areOpposite = (dir1, dir2) => {
@@ -125,10 +133,44 @@ const areOpposite = (dir1, dir2) => {
   return false;
 };
 
-drawSnake(currentSnake);
-// setInterval(() => {
+const checkValidHead = (positions, [top, left]) => {
+  /* if top or left position is less than 0 (outside the grid) it's not valid */
+  if (top < 0 || left < 0 ) {
+    return false
+  }
+  /* if top or left position is greater than the number of rows or columns, it isn't valid */
+  if (top >= ROWS || left >= COLS) {
+    return false
+  }
+  // we draw the current position
+  let position = top + '_' + left
+  if (positions.has(position)) {
+    return false;
+  }
+  /*if top and left are within the grid, it's valid head's position */
+  return true
+};
+
+function stopGame() {
+  // we add to the canvas border style red color
+  CANVAS.style.borderColor = "red";
+  clearInterval(gameInterval);
+}
+
+drawSnake();
+// let gameInterval = setInterval(() => {
 //   step()
 // }, 100);
+
+function toPositionSet(snake) {
+  let set = new Set();
+  for (let [top, left] of snake) {
+    let position = top + "_" + left;
+    set.add(position);
+  }
+  return set;
+}
+
 
 function dump(obj) {
   document.getElementById("debug").innerText = JSON.stringify(obj);
